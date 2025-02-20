4fb5fec7b73de268ff56870b4ca871e81fe343a0
Loki <thangavel@qutrix.io>
2025-02-20 22:48:54 +0530

    refiements

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,99 +1,76 @@
 // import  { Router } from 'express';
 // import express from express;
 
-const express = require('express');
-const { UserModel } = require('../models/user');
-const { generateToken } = require('../utils');
-const { authorized } = require('../middleware/auth');
+const express = require("express");
+const { UserModel } = require("../models/user");
+const { generateToken } = require("../utils");
+const authorized = require("../middleware/auth");
 
-const userRouter = express.Router()
+const userRouter = express.Router();
 
-// const userRouter = new Router(); 
+// Signup
 
+userRouter.post("/signup", async (req, res) => {
+  const { name, email, password } = req.body;
+  // validate
+  if (!name || !email || !password) {
+    res.status(404);
+    throw new Error("Please enter all feilds");
+  }
+  // check existing
+  const existingUser = await UserModel.findOne({ email: email });
 
+  if (existingUser) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
 
+  try {
+    const user = await UserModel.create({ name, email, password });
+    // create token
 
-// Signup  
+    const authToken = generateToken(user._id);
 
-userRouter.post("/signup", async(req, res) =>{
-    const {name ,  email , password} = req.body 
-    // validate  
-    if(!name || !email || !password){
-        res.status(404) 
-        throw new Error("Please enter all feilds")
-    }
-    // check existing 
-    const existingUser = await UserModel.findOne({email: email})
-
-    if(existingUser){
-        res.status(400) 
-        throw new Error("User already exists")
-    }
-
-
-try{
-    const user = await  UserModel.create({name, email, password});
-        // create token 
-
-    const authToken =    generateToken(user._id)
-    
-
-    res.status(201).json({...user, token});;
-} 
-
-catch(err){
+    res.status(201).json({ ...user, token });
+  } catch (err) {
     res.status(400);
     throw new Error("User not found");
-}
-}
-)
+  }
+});
 
+// Login
 
-// Login  
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("Missing req fileds");
+  }
 
-userRouter.post("/login", async(req, res)=>{
-    const {email , password} = req.body; 
-    if(!email || !password) {
-        res.status(400)
-        throw new Error("Missing req fileds")
-    }
+  const user = UserModel.findOne({ email });
 
-    const user = UserModel.findOne({email})
+  if (user && user.matchPassword(password)) {
+    const token = generateToken(user._id);
+    res.status(200).json({ ...user, token });
+  } else {
+    res.status(400);
+    throw new Error("Invalid credinetials");
+  }
+});
 
-    if(user && user.matchPassword(password)){
-        const token = generateToken(user._id)
-        res.status(200).json({...user, token});
-    }
-    else {
-        res.status(400) 
-        throw new Error("Invalid credinetials")
-    }
-})
+// Forget Password
 
-// Forget Password  
-
-
-
-userRouter.use(authorized)
-
+userRouter.use(authorized);
 
 // Profile Update ?
 
-
 userRouter.get("/all", (req, res) => {
-    console.log("Users")
-    res.send("Hello users")
-})
+  console.log("Users");
+  res.send("Hello users");
+});
 
+// Search users initally to get stateted
 
-// Search users initally to get stateted 
-
-
-
-
-userRouter.get("/users", async(req, res)=>{
-
-})
+userRouter.get("/users", async (req, res) => {});
 
 module.exports = userRouter;
-
--- server/server.js
+++ server/server.js
@@ -1,37 +1,27 @@
-const express = require('express')
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-const userRouter = require('./routes/user');
-const { notFound, errorHandler } = require('./middleware/error');
-const { authorized } = require('./middleware/auth');
-const app = express()
+const express = require("express");
+const dotenv = require("dotenv");
+const userRouter = require("./routes/user");
+const { notFound, errorHandler } = require("./middleware/error");
+const authorized = require("./middleware/auth");
+const { connectToDb } = require("./utils");
+const app = express();
 
-dotenv.config()
+dotenv.config();
 
+connectToDb();
 
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Yo World");
+});
 
+app.use("/api/v1/users", userRouter);
+app.use(authorized);
+// chats
+// messages
 
+app.use(notFound);
+app.use(errorHandler);
 
-connectToDb()
-
-app.use(express.json()); 
-
-
-app.get('/',  (req, res) =>{
-  res.send('Yo World')
-})
-
-
-
-app.use("/api/v1/users",userRouter)
-app.use(authorized)
-// chats  
-// messages 
-
-
-app.use(notFound) 
-app.get(errorHandler)
-
-
-app.listen('3000', console.log("Serevr running on port 3000"))
+app.listen("3000", console.log("Serevr running on port 3000"));
